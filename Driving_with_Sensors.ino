fee563621195b5cdec67434fdf07448507d86181
Jacob Lezberg <jacob.lezberg@gmail.com>
2014-11-17 18:36:28 -0500

    Big Update to Driving_with_Sensors.ino
    Completed basic approach, sense, and stop code (hopefully). Still need
    to implement the actual turning.

--- Driving_with_Sensors.ino
+++ Driving_with_Sensors.ino
@@ -1,23 +1,26 @@
-//MISSION: approach wall, stop, turn 90 degrees, proceed forward (parallel to wall)
+//MISSION: approach wall and stop without a collision. NEXT: turn 90 degrees, proceed forward (parallel to wall)
 
-const int motorPinA = 3, motorPinB = 11; //assigns motor control pins
-const int sideSensorPinTrigger = 7, sideSensorPinEcho = 6; //assigns side sensor control pins
-const int frontSensorPinTrigger = 5, frontSensorPinEcho = 4; //assigns front sensor control pins
-long rawDistance, distance;
+  const int motorSpeed = 150; //default speed for motors (in range 0-255)
+  const int motorPinA = 3, motorPinB = 11; //assigns motor control pins
+  const int frontSensorPinTrigger = 5, frontSensorPinEcho = 4; //assigns front sensor control pins
+  const int sideSensorPinTrigger = 7, sideSensorPinEcho = 6; //assigns side sensor control pins
+  long rawDistanceFront, distanceFront, rawDistanceSide, distanceSide; //variables for sensor values
+  boolean reverse = false; //should the motor run in reverse?
 
 void setup() {
   Serial.begin(9600); //sets up serial port
   pinMode(motorPinA, OUTPUT); //sets up first motor
   pinMode(motorPinB, OUTPUT); //second motor
-  pinMode(frontSensorPinTrigger, OUTPUT); //sets up sensor
-  pinMode(frontSensorPinEcho, INPUT); //sets up sensor feedback
-  //2nd sensor needed?
+  pinMode(frontSensorPinTrigger, OUTPUT); //sets up front sensor
+  pinMode(frontSensorPinEcho, INPUT); //front sensor feedback
+  pinMode(sideSensorPinTrigger, OUTPUT); //sets up side sensor
+  pinMode(sideSensorPinEcho, INPUT); //side sensor feedback
 }
 
 void loop() {
-  //drive forward
+  analogWrite(motorPinA, motorSpeed);
+  analogWrite(motorPinB, motorSpeed);
   checkForObstacle();
-  //turnClockwise90()
 }
 
 void checkForObstacle() { //mostly ripped from Ultrasonic file
@@ -25,12 +28,32 @@ void checkForObstacle() { //mostly ripped from Ultrasonic file
   digitalWrite(frontSensorPinTrigger, HIGH);
   delayMicroseconds(10); // short delay
   digitalWrite(frontSensorPinTrigger, LOW); //set trigger low
-  rawDistance = pulseIn(frontSensorPinEcho, HIGH); //read echo pin
-  distance = (rawDistance/2) / 29.1; // convert to cm
-  Serial.print(distance);
+  rawDistanceFront = pulseIn(frontSensorPinEcho, HIGH); //read echo pin
+  distanceFront = (rawDistanceFront/2) / 29.1; // convert to cm
+  Serial.print(distanceFront);
   Serial.println("cm");
+  
+  if (distanceFront <= 20) {
+    digitalWrite(motorPinA, LOW); //stop motor A
+	digitalWrite(motorPinB, LOW); //stop motor B
+	turn90Degrees(); //now that we're stopped, turn to avoid the obstacle
+	}
 }
 
-void turnClockwise90() {
-  //turn right wheel back, left wheel forward
+void turn90Degrees() {
+  //check for obstacle with side sensor
+  digitalWrite(sideSensorPinTrigger, HIGH);
+  delayMicroseconds(10); // short delay
+  digitalWrite(sideSensorPinTrigger, LOW); //set trigger low
+  rawDistanceSide = pulseIn(sideSensorPinEcho, HIGH); //read echo pin
+  distanceSide = (rawDistanceSide/2) / 29.1; // convert to cm
+  if (distanceSide <= 20) { //check for a wall on the left
+	Serial.print(distanceSide);
+	Serial.println("HOLY S*** THERE'S SOMETHING ON THE LEFT! TURN RIGHT!");
+	//turn right
+	} else {
+	Serial.print(distanceSide);
+	Serial.println("Nothing on the left, turning that way.");
+	//turn left
+	}
 }
