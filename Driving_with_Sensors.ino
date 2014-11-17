25e3ba90361b8939b76a1673bd69aa46d764f99f
PHSRoboticsClub <PHSRoboticsClub@users.noreply.github.com>
2014-11-17 14:59:05 -0500

    Update Driving_with_Sensors.ino

--- Driving_with_Sensors.ino
+++ Driving_with_Sensors.ino
@@ -1,31 +1,34 @@
 //MISSION: approach wall, stop, turn 90 degrees, proceed forward (parallel to wall)
 
 const int motorPinA = 3, motorPinB = 11; //assigns motor control pins
-const int sensorPinTrigger = 7, sensorPinEcho = 6; //assigns sensor control pins
+const int sideSensorPinTrigger = 7, sideSensorPinEcho = 6; //assigns side sensor control pins
+const int frontSensorPinTrigger = 5, frontSensorPinEcho = 4; //assigns front sensor control pins
 long rawDistance, distance;
 
 void setup() {
   Serial.begin(9600); //sets up serial port
   pinMode(motorPinA, OUTPUT); //sets up first motor
   pinMode(motorPinB, OUTPUT); //second motor
-  pinMode(sensorPinTrigger, OUTPUT); //sets up sensor
-  pinMode(sensorPinEcho, INPUT); //sets up sensor feedback
+  pinMode(frontSensorPinTrigger, OUTPUT); //sets up sensor
+  pinMode(frontSensorPinEcho, INPUT); //sets up sensor feedback
   //2nd sensor needed?
 }
 
 void loop() {
   //drive forward
-  //checkForObstacle()
+  checkForObstacle();
   //turnClockwise90()
 }
 
 void checkForObstacle() { //mostly ripped from Ultrasonic file
   //if sensor value <= certain value, stop to avoid collision
-  digitalWrite(sensorPinTrigger, HIGH);
+  digitalWrite(frontSensorPinTrigger, HIGH);
   delayMicroseconds(10); // short delay
-  digitalWrite(sensorPinTrigger, LOW); //set trigger low
-  rawDistance = pulseIn(sensorPinEcho, HIGH); //read echo pin
+  digitalWrite(frontSensorPinTrigger, LOW); //set trigger low
+  rawDistance = pulseIn(frontSensorPinEcho, HIGH); //read echo pin
   distance = (rawDistance/2) / 29.1; // convert to cm
+  Serial.print(distance);
+  Serial.println("cm");
 }
 
 void turnClockwise90() {
