de349c4c34c7efd35a9e01fb381b09ec39502f76
Jacob Lezberg <jacob.lezberg@gmail.com>
2014-11-30 10:44:25 -0500

    Restructure
    Arduino IDE wants each file inside a project folder. I complied.

--- Driving_with_Sensors/Driving_with_Sensors.ino
+++ Driving_with_Sensors/Driving_with_Sensors.ino
@@ -1,11 +1,10 @@
 //MISSION: approach wall and stop without a collision. NEXT: turn 90 degrees, proceed forward (parallel to wall)
 
-  const int motorSpeed = 150; //default speed for motors (in range 0-255)
+  const int motorSpeed = 50; //default speed for motors (in range 0-255)
   const int motorPinA = 3, motorPinB = 11; //assigns motor control pins
   const int frontSensorPinTrigger = 5, frontSensorPinEcho = 4; //assigns front sensor control pins
   const int sideSensorPinTrigger = 7, sideSensorPinEcho = 6; //assigns side sensor control pins
   long rawDistanceFront, distanceFront, rawDistanceSide, distanceSide; //variables for sensor values
-  boolean reverse = false; //should the motor run in reverse?
 
 void setup() {
   Serial.begin(9600); //sets up serial port
@@ -35,9 +34,10 @@ void checkForObstacle() { //mostly ripped from Ultrasonic file
   
   if (distanceFront <= 20) {
     digitalWrite(motorPinA, LOW); //stop motor A
-	digitalWrite(motorPinB, LOW); //stop motor B
-	turn90Degrees(); //now that we're stopped, turn to avoid the obstacle
-	}
+    digitalWrite(motorPinB, LOW); //stop motor B
+    delay(100);
+    turn90Degrees(); //now that we're stopped, turn to avoid the obstacle
+    }
 }
 
 void turn90Degrees() {
@@ -47,13 +47,19 @@ void turn90Degrees() {
   digitalWrite(sideSensorPinTrigger, LOW); //set trigger low
   rawDistanceSide = pulseIn(sideSensorPinEcho, HIGH); //read echo pin
   distanceSide = (rawDistanceSide/2) / 29.1; // convert to cm
+  
   if (distanceSide <= 20) { //check for a wall on the left
-	Serial.print(distanceSide);
 	Serial.println("HOLY S*** THERE'S SOMETHING ON THE LEFT! TURN RIGHT!");
 	//turn right
-	} else {
-	Serial.print(distanceSide);
+        analogWrite(motorPinB, motorSpeed);
+        delay(500);
+        //use millis() to calculate elapsed time for delay before loop() drives forward
+  } else {
+	//Serial.print(distanceSide);
 	Serial.println("Nothing on the left, turning that way.");
 	//turn left
-	}
+        analogWrite(motorPinA, motorSpeed);
+        delay(500);
+        //use millis() to calculate elapsed time for delay before loop() drives forward
+  }
 }
